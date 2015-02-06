ed5eeebd2682fa408ebb31b9680f8d7825dc142a
BFriedland <friedland.ben@gmail.com>
2015-02-06 15:14:01 -0800

    Copied over read_entries

--- pyramid_learning_journal/journal.py
+++ pyramid_learning_journal/journal.py
@@ -26,6 +26,10 @@ INSERT_ENTRY = """
 INSERT INTO entries (title, text, created) VALUES (%s, %s, %s)
 """
 
+SELECT_ENTRIES = """
+SELECT id, title, text, created FROM entries ORDER BY created DESC
+"""
+
 
 logging.basicConfig()
 log = logging.getLogger(__file__)
@@ -129,6 +133,17 @@ def write_entry(request):
     text = request.params.get('text', None)
     created = datetime.datetime.utcnow()
     request.db.cursor().execute(INSERT_ENTRY, [title, text, created])
+
+
+def read_entries(request):
+    """
+    Return a list of all entries as dictionaries.
+    """
+    cursor = request.db.cursor()
+    cursor.execute(SELECT_ENTRIES)
+    keys = ('id', 'title', 'text', 'created')
+    entries = [dict(zip(keys, row)) for row in cursor.fetchall()]
+    return {'entries': entries}
 
 
 if __name__ == '__main__':
